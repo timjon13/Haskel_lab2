08ea9095fbe019b96a5faaed124fbe3e339e85e5
Adam Grandén <adam.granden@hotmail.com>
2014-09-09 20:03:06 +0200

    Update haskellab.hs

--- haskellab.hs
+++ haskellab.hs
@@ -1,6 +1,6 @@
 module BlackJack where
-import Cards
-import Wrapper
+import Cards.hs
+import Wrappers.hs
 import Test.QuickCheck
 
 aCard1::Card
@@ -14,7 +14,7 @@ empty=Empty
 
 valueRank::Rank->Integer
 valueRank (Numeric i) = i
-valueRank Ace         = 11 --ToDo || check if its one
+valueRank Ace         = 11 || 1  --ToDo || check if its one
 valueRank King        = 10
 valueRank Queen       = 10
 valueRank Jack        = 10
@@ -22,12 +22,17 @@ valueRank Jack        = 10
 valueCard::Card->Integer
 valueCard (Card r c) = valueRank v
 
-numberOfAces::Hand->Integer
+numberOfAces::Hand->Integer --ToDo || Check for all Aces
 numberOfAces Empty = 0
 numberOfAces
 numberOfAces
 
-value::Hand->Interger
+
+valueofhand::Hand->Interger --ToDo Check the value for the hand
+valueofhand Empty = 0
+valueofhand 
+
+value::Hand->Interger --ToDo Check value of all cards in the hand 
 value hand| 
 	  | otherwise valueofhand
 
@@ -40,4 +45,3 @@ winner handOfGuest handOfBank | gameover(handOfGuest) = Bank
                               | handOfGuest < handOfBank = Bank
                               | handOfGuest > handOfBank = Guest
                               | handOfGuest == handOfBank= Bank
-
